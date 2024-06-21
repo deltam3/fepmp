dc5cfcbe496bfbcd0ac2773a185144ecf452faf5
deltam3 <practicect@proton.me>
2024-06-21 11:19:24 +0900

    problemlist and Link to FAQ page

---
+++ fepmp/app/_components/ProblemList.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const ProblemList = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link href="/frontendeval/easy/faq">FAQ Component</Link>
+          <p>Reusable Accordion Component</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ProblemList;
--- fepmp/app/frontendeval/page.tsx
+++ fepmp/app/frontendeval/page.tsx
@@ -1,7 +1,14 @@
-export default function Page({}) {
+import { Suspense } from "react";
+import ProblemList from "../_components/ProblemList";
+
+export default function Page() {
   return (
     <div>
       <h1>Practice Frontend Skills</h1>
+
+      <Suspense>
+        <ProblemList />
+      </Suspense>
     </div>
   );
 }
