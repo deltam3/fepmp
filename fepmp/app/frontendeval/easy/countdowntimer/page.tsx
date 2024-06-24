1624808a128151e9cc4161073213b36276c479e3
deltam3 <practicect@proton.me>
2024-06-24 10:39:46 +0900

    basic countdown timer layout

--- fepmp/app/frontendeval/easy/countdowntimer/page.tsx
+++ fepmp/app/frontendeval/easy/countdowntimer/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import Input from "@/app/_ui/Input/Input";
+
 const Page = () => {
   return (
     <div>
       <h1>Countdown Timer</h1>
+      <div>
+        <Input />
+        <span>:</span>
+        <Input />
+        <span>:</span>
+        <Input />
+      </div>
     </div>
   );
 };
