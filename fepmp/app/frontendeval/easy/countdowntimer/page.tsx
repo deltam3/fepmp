b712f3d23b9fbed89f6f559931c949bcd3671907
deltam3 <practicect@proton.me>
2024-06-25 11:03:23 +0900

    added functionality to input time

--- fepmp/app/frontendeval/easy/countdowntimer/page.tsx
+++ fepmp/app/frontendeval/easy/countdowntimer/page.tsx
@@ -1,19 +1,68 @@
 "use client";
-import React, { useState } from "react";
+import React, { FormEvent, ChangeEvent, useState } from "react";
 import Button from "@/app/_ui/Button/Button";
 import Input from "@/app/_ui/Input/Input";
 
 const Page = () => {
-  const [isStart, setIsStart] = useState(false);
+  const [isStart, setIsStart] = useState<boolean>(false);
+
+  const [hours, setHours] = useState<number | "HH">("HH");
+  const [minutes, setMinutes] = useState<number | "MM">("MM");
+  const [seconds, setSeconds] = useState<number | "SS">("SS");
+
+  const hoursHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    let newValue: number = parseInt(e.target.value);
+    if (isNaN(newValue)) newValue = 0;
+    setHours(newValue);
+  };
+
+  const minutesHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    let newValue: number = parseInt(e.target.value);
+    if (isNaN(newValue)) newValue = 0;
+    setMinutes(newValue);
+  };
+
+  const secondsHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    let newValue: number = parseInt(e.target.value);
+    if (isNaN(newValue)) newValue = 0;
+    setSeconds(newValue);
+  };
+
   return (
     <div>
       <h1>Countdown Timer</h1>
       <div>
-        <Input placeholder="HH" />
+        {isStart ? (
+          <span>{hours}</span>
+        ) : (
+          <Input
+            value={hours}
+            type="text"
+            onChange={(e: ChangeEvent<HTMLInputElement>) => hoursHandler(e)}
+          />
+        )}
         <span>:</span>
-        <Input placeholder="MM" />
+        {isStart ? (
+          <span>{minutes}</span>
+        ) : (
+          <Input
+            value={minutes}
+            type="text"
+            onChange={(e: ChangeEvent<HTMLInputElement>) => minutesHandler(e)}
+          />
+        )}
+
         <span>:</span>
-        <Input placeholder="SS" />
+        {isStart ? (
+          <span>{seconds}</span>
+        ) : (
+          <Input
+            value={seconds}
+            type="text"
+            onChange={(e: ChangeEvent<HTMLInputElement>) => secondsHandler(e)}
+          />
+        )}
+
         <span>
           {isStart ? (
             <>
