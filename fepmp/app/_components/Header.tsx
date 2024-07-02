0a61b3340ae3ae6a4d7af8550768305201b45785
deltam3 <practicect@proton.me>
2024-07-02 09:35:28 +0900

    made adjustments to the title

--- fepmp/app/_components/Header.tsx
+++ fepmp/app/_components/Header.tsx
@@ -43,15 +43,15 @@ const Header = () => {
             className="flex flex-row space-x-3 items-center justify-center md:hidden"
           >
             <span className="h-7 w-7 bg-zinc-300 rounded-lg" />
-            <span className="font-bold text-xl flex ">Logo</span>
+            <span className="font-bold text-xl flex ">FePMP</span>
           </Link>
         </div>
 
-        <div className="hidden md:block">
+        {/* <div className="hidden md:block">
           <div className="h-8 w-8 rounded-full bg-zinc-300 flex items-center justify-center text-center">
             <span className="font-semibold text-sm">HQ</span>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
