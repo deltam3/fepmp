afd76550415bf3abf0fba0daa6b1019dcd8dc24d
deltam3 <practicect@proton.me>
2024-07-02 10:46:08 +0900

    changed the problemlist so that problemslist is mapped from ProblemsList variable

---
+++ fepmp/app/_components/ProblemItem.tsx
@@ -0,0 +1,52 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+interface Props {
+  item: {
+    link: string;
+    title: string;
+    description: string;
+  };
+}
+
+const moveUpAnimation = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(-10px);
+  }
+`;
+
+const StyledProblemItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1.6rem;
+  gap: 1.6rem;
+  border: 1.6rem solid var(--color-grey-900);
+
+  &:hover {
+    background-color: var(--color-grey-200);
+    cursor: pointer;
+    animation: ${moveUpAnimation} 0.3s forwards;
+  }
+`;
+
+const ProblemItem = ({ item: { link, title, description } }: Props) => {
+  const router = useRouter();
+  const onClickHandler = () => {
+    router.push(`${link}`);
+  };
+  return (
+    <StyledProblemItem onClick={onClickHandler}>
+      <Link href={link}>{title}</Link>
+      <p>{description}</p>
+    </StyledProblemItem>
+  );
+};
+
+export default ProblemItem;
--- fepmp/app/_components/ProblemList.tsx
+++ fepmp/app/_components/ProblemList.tsx
@@ -1,30 +1,41 @@
 import Link from "next/link";
+import ProblemItem from "./ProblemItem";
+
+const ProblemsList = [
+  {
+    id: 1,
+    link: "/frontendeval/easy/faq",
+    title: "FAQ Component",
+    description: "Reusable Accordion Component",
+  },
+  {
+    id: 2,
+    link: "/frontendeval/easy/countdowntimer",
+    title: "Countdown Timer",
+    description:
+      "Countdown Timer that allows the user to enter hours, minutes, and seconds",
+  },
+  {
+    id: 3,
+    link: "/frontendeval/easy/modal",
+    title: "Modal",
+    description: "A reusable Modal",
+  },
+  {
+    id: 4,
+    link: "/frontendeval/easy/mortgagecalculator",
+    title: "Mortgage Calculator",
+    description: "월별 주택담보 대출 상환액을 계산하는 계산기",
+  },
+];
 
 const ProblemList = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <Link href="/frontendeval/easy/faq">FAQ Component</Link>
-          <p>Reusable Accordion Component</p>
-        </li>
-        <li>
-          <Link href="/frontendeval/easy/countdowntimer">Countdown Timer</Link>
-          <p>
-            Countdown Timer that allows the user to enter hours, minutes, and
-            seconds
-          </p>
-        </li>
-        <li>
-          <Link href="/frontendeval/easy/modal">Modal</Link>
-          <p>A reusable Modal</p>
-        </li>
-        <li>
-          <Link href="/frontendeval/easy/mortgagecalculator">
-            Mortgage Calculator
-          </Link>
-          <p>월별 주택담보 대출 상환액을 계산하는 계산기 </p>
-        </li>
+      <ul className="grid grid-cols-3 gap-[1.6rem]">
+        {ProblemsList.map((item) => {
+          return <ProblemItem key={item.id} item={item}></ProblemItem>;
+        })}
       </ul>
     </div>
   );
