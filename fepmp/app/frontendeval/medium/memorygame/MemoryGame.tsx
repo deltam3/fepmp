33f00340504127bc044e7fdb15cae7af308941e1
deltam3 <practicect@proton.me>
2024-07-14 14:45:20 +0900

    created MemoryGame component

---
+++ fepmp/app/frontendeval/medium/memorygame/MemoryGame.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const MemoryGame = (props: Props) => {
+  return <div>MemoryGame</div>;
+};
+
+export default MemoryGame;
--- fepmp/app/frontendeval/medium/memorygame/page.tsx
+++ fepmp/app/frontendeval/medium/memorygame/page.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+"use client";
+import Button from "@/app/_ui/Button/Button";
+import React, { useState } from "react";
+import MemoryGame from "./MemoryGame";
 
-type Props = {};
+// function setGameItems () {
+//   if
 
-const page = (props: Props) => {
-  return <div>page</div>;
+// }
+
+const fivebyfive = [];
+const sixbysix = [];
+const eightbyeight = [];
+
+const page = () => {
+  const [gameItems, setGameItems] = useState([{}]);
+  const [isStart, setIsStart] = useState();
+  return (
+    <>
+      <h1>Memory Game</h1>
+      <div>
+        <div>
+          <Button>Easy 5*5</Button>
+          <Button>Medium 6*6</Button>
+          <Button>Hard 8*8</Button>
+        </div>
+      </div>
+      <main>
+        <MemoryGame />
+      </main>
+    </>
+  );
 };
 
 export default page;
