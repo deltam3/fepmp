d5121cfe5cf7c92117b93fef3aa2370d6e396167
deltam3 <practicect@proton.me>
2024-07-16 10:08:58 +0900

    apply different layout based on the difficulty chosen

--- fepmp/app/frontendeval/medium/memorygame/MemoryGame.tsx
+++ fepmp/app/frontendeval/medium/memorygame/MemoryGame.tsx
@@ -1,26 +1,70 @@
 import React from "react";
 import MemoryGameItem from "./MemoryGameItem";
+import styled from "styled-components";
 type Props = {
   gameItems: any;
 };
 
+const StyledEasyLayout = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+  gap: 10px;
+  width: 500px;
+  height: 500px;
+`;
+
+const StyledMediumLayout = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: repeat(6, 1fr);
+  gap: 10px;
+  width: 500px;
+  height: 500px;
+`;
+
+const StyledHardLayout = styled.div`
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: repeat(8, 1fr);
+  gap: 10px;
+  width: 500px;
+  height: 500px;
+`;
+
 const MemoryGame = ({ gameItems }: Props) => {
   const gameLength = gameItems.length;
-  if (gameLength === 25) {
-    console.log(25);
-  } else if (gameLength === 36) {
-    console.log(36);
-  } else if (gameLength == 64) {
-    console.log(64);
-  }
+  const isEasy = gameLength == 16;
+  const isMedium = gameLength == 36;
+  const isHard = gameLength == 64;
 
   return (
     <>
       <h1>MemoryGameItems {gameItems.length}</h1>
-      <section className="grid">
-        {gameItems.map((item) => {
-          return <MemoryGameItem key={item.id} item={item}></MemoryGameItem>;
-        })}
+      <section>
+        {isEasy && (
+          <StyledEasyLayout>
+            {gameItems.map((item) => (
+              <MemoryGameItem key={item.id} item={item}></MemoryGameItem>
+            ))}
+          </StyledEasyLayout>
+        )}
+
+        {isMedium && (
+          <StyledMediumLayout>
+            {gameItems.map((item) => (
+              <MemoryGameItem key={item.id} item={item}></MemoryGameItem>
+            ))}
+          </StyledMediumLayout>
+        )}
+
+        {isHard && (
+          <StyledHardLayout>
+            {gameItems.map((item) => (
+              <MemoryGameItem key={item.id} item={item}></MemoryGameItem>
+            ))}
+          </StyledHardLayout>
+        )}
       </section>
     </>
   );
